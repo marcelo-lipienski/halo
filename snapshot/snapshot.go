de41bdd5db8702244f99a8bfed1db6f73f9aff3f
Marcelo Gato Lipienski <marcelo.lipienski@gmail.com>
2026-07-20 23:08:54 -0300

    fix: ignore Close error in computeSHA256 to fix errcheck linter

--- snapshot/snapshot.go
+++ snapshot/snapshot.go
@@ -60,7 +60,7 @@ func computeSHA256(path string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer f.Close()
+	defer func() { _ = f.Close() }()
 	h := sha256.New()
 	if _, err := io.Copy(h, f); err != nil {
 		return "", err
